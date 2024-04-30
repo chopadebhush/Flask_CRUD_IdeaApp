cfa9409b8a1cc1d47e2e88113278db795dc4bb54
Bhushan <chopadebhush@gmail.com>
2024-04-30 21:38:33 +0530

    Ideaapp V1

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask,request
+
+app =Flask(__name__)
+#Create the idea repository. This where ideas will be stored
+ideas  = {
+    1 : {
+        "id" : 1,
+        "idea_name" : "ONDC",
+        "idea_description" : "Details about ONDC",
+        "idea_author" : "Adeeb"
+    },
+    2 : {
+        "id" : 2,
+        "idea_name" : "Save Soil",
+        "idea_description" : "Details about Saving Soil",
+        "idea_author" : "Ankit Sharma"
+    }
+
+}
+
+'''
+Creat an RESTful endpoint for fetching all the ideas
+'''
+@app.get("/ideaapp/api/v1/ideas")
+def get_all_ideas():
+    #Logic to fetch all the ideas
+    return ideas
+
+
+'''
+Creat an RESTful APi creating all the ideas if it is repeated
+it will not create
+'''
+
+@app.post("/ideaapp/api/v1/ideas")
+def create_idea():
+    #logic to create a new idea
+    try :
+        #first read the request body
+        request_body = request.get_json()
+        #Check if the idea id passed is not present already
+        if request_body["id"] and request_body["id"] in ideas :
+            return "idea with the same id already present",400
+
+        #Insert the passed idea in the ideas dictionary
+        ideas[request_body["id"]] = request_body
+
+        #return the response saying idea got saved
+        return "idea created and saved successfully",201
+    except KeyError :
+        return "id is missing",400
+    except :
+        return "Some internal server error", 500
+
+'''
+Creat an RESTful endpoint for fetching all the ideas
+'''
+
+
+
+
+
+
+
+
+
+    
+    
+
+if __name__ == '__main__':
+    app.run(port=8080)
+    
+
